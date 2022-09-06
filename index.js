d050d410efb6ce6265803234230cdd7c7f45bff7
Dennis de Klerk <dmdeklerk@gmail.com>
2022-09-06 14:13:08 +0200

    Add string to hex conversion

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { mnemonicToSeedSync, generateMnemonic, validateMnemonic } = require('bip39')
 const { fromSeed, } = require('bip32')
 const { getPublicKeyFromPrivateKey, getAccountIdFromPublicKey } = require('heat-sdk-v2/dist/crypto')
+const { stringToHexString } = require('heat-sdk-v2/dist/converters')
 
 /**
  * Notice how we use 286 here for private key derivation.
@@ -8,18 +9,20 @@ const { getPublicKeyFromPrivateKey, getAccountIdFromPublicKey } = require('heat-
  * as you can see Heat has 286 registered here https://github.com/satoshilabs/slips/blob/master/slip-0044.md
  * See that document for options to register your own bip44 coin code.
  */
-function mnemonicToHeatPrivateKey(mnemonic) {
+function mnemonicToPrivateKey(mnemonic, network) {
+  network = network || 286
   if (!validateMnemonic(mnemonic))
     throw new Error('Invalid mnemonic')
   const seed = mnemonicToSeedSync(mnemonic)
   const root = fromSeed(seed)
-  const node = root.derivePath("m/44'/286'/0'/0/0")
+  const node = root.derivePath(`m/44'/${network}'/0'/0/0`)
   return node.privateKey.toString('hex')
 }
 
 module.exports = {
   generateMnemonic,
-  mnemonicToHeatPrivateKey,
+  mnemonicToPrivateKey,
+  stringToHexString,
   getPublicKeyFromPrivateKey,
   getAccountIdFromPublicKey
 }
--- test/test.js
+++ test/test.js
@@ -1,20 +1,23 @@
 const { generateMnemonic,
-  mnemonicToHeatPrivateKey,
+  mnemonicToPrivateKey,
+  stringToHexString,
   getPublicKeyFromPrivateKey,
   getAccountIdFromPublicKey } = require('../index')
 
 var assert = require('assert');
 describe('Seed, private key, public key, address', function() {
-  it('should work', function() {
+
+  it('Generate unique mnemonic + privatekey + publickey + address', function() {
     const mnemonic = generateMnemonic()
     assert.equal(mnemonic.split(' ').length, 12)
     console.log('Mnemonic: ' + mnemonic)
 
-    const privateKey = mnemonicToHeatPrivateKey(mnemonic)
+    const privateKey = mnemonicToPrivateKey(mnemonic, /* 726683 */)
     assert.equal(privateKey.length, 64)
     console.log('Private key: ' + privateKey)
 
-    const publicKey = getPublicKeyFromPrivateKey(privateKey)
+    const privateKeyHex = stringToHexString(privateKey)
+    const publicKey = getPublicKeyFromPrivateKey(privateKeyHex)
     assert.equal(publicKey.length, 64)
     console.log('Public key: ' + publicKey)
 
@@ -22,4 +25,26 @@ describe('Seed, private key, public key, address', function() {
     assert(address.length > 1)
     console.log('Address: ' + address)
   });
+
+  it('does Harmony test', function() {
+    const mnemonic = 'blame case champion horror airport absurd evoke vessel like rough gift slight';
+    assert.equal(mnemonic.split(' ').length, 12)
+    console.log('Mnemonic: ' + mnemonic)
+
+    const privateKey = mnemonicToPrivateKey(mnemonic, 726683)
+    assert.equal(privateKey.length, 64)
+    assert.equal(privateKey, '9d3738fe074fcdfed1d3a1891340172ca670c3860af2666570fff0cb88497aed')
+    console.log('Private key: ' + privateKey)
+
+    const privateKeyHex = stringToHexString(privateKey)
+    const publicKey = getPublicKeyFromPrivateKey(privateKeyHex)
+    assert.equal(publicKey.length, 64)
+    assert.equal(publicKey, 'b7d8a0212130ec519dea029d7a6fbbc180205a9cdb1c3743e6659d3258bd7a0f')
+    console.log('Public key: ' + publicKey)
+
+    const address = getAccountIdFromPublicKey(publicKey)
+    assert(address.length > 1)
+    assert.equal(address, '8448398312458207917')
+    console.log('Address: ' + address)
+  });  
 });
